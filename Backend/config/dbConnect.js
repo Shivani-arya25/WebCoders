96e4c9e94b24a1df95674e4ef0c8956efba7130c
ShrashtiTiwari <96613830+ShrashtiTiwari@users.noreply.github.com>
2022-01-31 14:28:47 +0530

    dbConnect.js

--- Backend/config/dbConnect.js
+++ Backend/config/dbConnect.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
 
+const dbConnect = () => {
+// connect DB
+mongoose
+.connect(
+    'mongodb+srv://Webcoders:xnzBPZJopiq3QyPq@cluster0.6u2uf.mongodb.net/DigitalLibrary', 
+    { 
+    
+    useUnifiedTopology: true,
+    
+    useNewUrlParser: true,
+    }
+)
+.then(() => console.log('DB Connected'))
+.catch(err => console.log(err));
+};
+
+module.exports = dbConnect;
