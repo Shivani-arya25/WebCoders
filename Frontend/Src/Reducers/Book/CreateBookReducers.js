7258165ba0ac49257416007b6b259164b0cae575
Aditi Harinkhere <73841069+Aditi20ah@users.noreply.github.com>
2022-03-02 13:49:22 +0530

    Update CreateBookReducers.js

--- Frontend/Src/Reducers/Book/CreateBookReducers.js
+++ Frontend/Src/Reducers/Book/CreateBookReducers.js
@@ -1,1 +1,25 @@
+import {CREATE_BOOK_REQUEST} from '../../actions/actions/'
 
+const CreateBookReducer =(State={}, action) => {
+    switch(action,type) {
+        case CREATE_BOOK_REQUEST;
+        return {
+            loading: true,
+        };
+        case CREATE_BOOK_SUCCESS:
+            return {
+                book: action.payload,
+            };
+
+            case CREATE_BOOK_FAIL:
+                return {
+                    loading: false,
+                    error: action.payload,
+                };
+
+             default:
+                 return State;
+    }
+};
+
+export { CreateBookReducer };
