8f38ec0cd6f5382d82e141040d523806ed88c0bf
Aditi Harinkhere <73841069+Aditi20ah@users.noreply.github.com>
2022-03-02 13:28:01 +0530

    Update BookAction.js

--- Frontend/Src/Action/Books/BookAction.js
+++ Frontend/Src/Action/Books/BookAction.js
@@ -1,1 +1,31 @@
+import axios from 'axios';app
+CREATE_BOOK_SUCCESS,
+} from '../actionTypes';
 
+const cresteBookAction =bookData => {
+return async dispatch => {
+    try{
+
+        dispatch({
+            type: 'CREATE_BOOK-REQUEST',
+        });
+    
+        const config =
+        'content-type': 'application/json',
+    };
+        const { data } = await axios.post('/api/books', booksData, config);
+    
+          dispatch({
+                type: CREATE_BOOK_SUCCESS,
+                payload: data,
+    });
+}  catch (error) {}
+     dispatch({
+         type: CREATE_BOOK_FAIL,
+         payload: error.response && error.response.data.massage
+     });
+    }  
+     };
+    };
+
+    export {createBookAction};
